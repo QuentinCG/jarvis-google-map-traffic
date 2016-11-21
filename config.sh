da5cb445b2e9b9bbbb5f37f02f067ecb8df8d7da
Aelios <lefevre.nicolas88@gmail.com>
2016-11-21 16:19:28 +0000

    help in config file

--- config.sh
+++ config.sh
@@ -1,4 +1,5 @@
 #pour obtenir une clé gratuite d'api : https://developers.google.com/maps/documentation/directions/get-api-key?hl=fr 
 APIKEY=""
+#You can enter complete addresses
 FROM="EPERNON"
 TO="PARIS"
