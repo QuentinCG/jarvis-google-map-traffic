1b683b10249762c816373c47173dc09883cebe37
Aelios <lefevre.nicolas88@gmail.com>
2016-11-21 16:03:00 +0000

    remove api key

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
 #pour obtenir une clé gratuite d'api : https://developers.google.com/maps/documentation/directions/get-api-key?hl=fr 
-APIKEY="AIzaSyDdjqdseIYNRstJk9lhL7LRkMoCbKDAONQ"
+APIKEY=""
 FROM="EPERNON"
 TO="PARIS"
