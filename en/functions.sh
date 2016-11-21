4a153bbeabfe9326d0532e798fa633409b290699
Aelios <lefevre.nicolas88@gmail.com>
2016-11-21 16:52:26 +0000

    url encode params

--- en/functions.sh
+++ en/functions.sh
@@ -7,7 +7,7 @@
 # pg for PluGin
 # XX can be a two letters code for your plugin, ex: ww for Weather Wunderground
 jv_pg_gm_check_traffic() {
-local json=$(curl -s "https://maps.googleapis.com/maps/api/directions/json?language=en&origin=$FROM&destination=$TO&traffic_model=best_guess&departure_time=now&key=$APIKEY")
+local json=$(curl -s -G "https://maps.googleapis.com/maps/api/directions/json" --data "language=en" --data-urlencode "origin=$FROM" --data-urlencode "destination=$TO" --data "traffic_model=best_guess" --data "departure_time=now" --data-urlencode "key=$APIKEY")
 local duration=$(echo "$json" | jq -r '.routes[0].legs[0].duration.value')
 local duration_in_traffic=$(echo "$json" | jq -r '.routes[0].legs[0].duration_in_traffic.value')
 local duration_in_traffic_text=$(echo "$json" | jq -r '.routes[0].legs[0].duration_in_traffic.text')
