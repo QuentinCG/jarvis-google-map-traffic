ed64267fea32487de989f5e0c0665aa21fa1fa4b
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-27 13:52:29 +0200

    plugin: Have utf8 without bom encoded file

--- fr/functions.sh
+++ fr/functions.sh
@@ -3,7 +3,7 @@
 jv_pg_gm_lang()
 {
   case "$1" in
-    say_traffic) echo "Le trajet entre $2 et $3 via $4 est estimé à $5 dont $6 minutes de retard.";;
+    say_traffic) echo "Le trajet entre $2 et $3 via $4 est estimÃ© Ã  $5 dont $6 minutes de retard.";;
     invalid_api_key) jv_error "Clef API Google Map invalide.";;
     *) jv_error "Erreur: Clef de traduction '$1' non reconnue. Merci de contacter le concepteur de ce plugin.";;
   esac
