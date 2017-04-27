63e4a9b4cf3e879bc3f0c7ab4336a9e907dc124c
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-27 14:12:15 +0200

    plugin: Fix language issue

--- functions.sh
+++ functions.sh
@@ -3,10 +3,11 @@
 # Say time and delay to go from $1 to $2
 # $1 (string): From
 # $2 (string): To
+# $3 (string): Language
 jv_pg_gm_check_traffic()
 {
   if [[ -n "$var_jv_pg_gm_api_key" ]]; then
-    local json=$(curl -s -G "https://maps.googleapis.com/maps/api/directions/json" --data "language=fr" --data-urlencode "origin=$1" --data-urlencode "destination=$2" --data "traffic_model=best_guess" --data "departure_time=now" --data-urlencode "key=$var_jv_pg_gm_api_key")
+    local json=$(curl -s -G "https://maps.googleapis.com/maps/api/directions/json" --data "language=$3" --data-urlencode "origin=$1" --data-urlencode "destination=$2" --data "traffic_model=best_guess" --data "departure_time=now" --data-urlencode "key=$var_jv_pg_gm_api_key")
     local duration=$(echo "$json" | jq -r '.routes[0].legs[0].duration.value')
     local duration_in_traffic=$(echo "$json" | jq -r '.routes[0].legs[0].duration_in_traffic.value')
     local duration_in_traffic_text=$(echo "$json" | jq -r '.routes[0].legs[0].duration_in_traffic.text')
