1fb393bc8b2d2916c388b9c2822537f2155be67c
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-27 13:46:15 +0200

    plugin en: Fix english error

--- en/functions.sh
+++ en/functions.sh
@@ -3,7 +3,7 @@
 jv_pg_gm_lang()
 {
   case "$1" in
-    say_traffic) echo "The route for $2 to $3 via $4 is $5 including $6 minutes late.";;
+    say_traffic) echo "The route from $2 to $3 via $4 is $5 including $6 minutes late.";;
     invalid_api_key) jv_error "Invalid Google Map API key.";;
     *) jv_error "Erreur: Clef de traduction '$1' non reconnue. Merci de contacter le concepteur de ce plugin.";;
   esac
